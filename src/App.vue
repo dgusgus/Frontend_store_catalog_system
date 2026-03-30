1f28ab70b509a81705949bab9dd44c1339a36d86
dgusgus <dgusgus1@gmail.com>
2026-03-30 08:44:44 -0400

    agreamos una estructura de carpetas al sistema

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,7 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import { useCounterStore } from './stores/counter'
-const counter = useCounterStore()
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <HelloWorld />
-  <div class="p-10">
-    <button class="btn btn-primary">Hola DaisyUI 🚀</button>
-  </div>
-  <router-view />
-  <div class="p-10">
-    <h1 class="text-xl">Contador: {{ counter.count }}</h1>
-
-    <button class="btn btn-primary mt-4" @click="counter.increment()">
-      Incrementar
-    </button>
-  </div>
+<RouterView />
 </template>
